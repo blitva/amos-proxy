c1641664e91998d2869df523108cf5441e8b07b4
amosNash <66794449+amosNash@users.noreply.github.com>
2021-02-03 11:56:18 -0700

    Able to get multiple components on page

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require('cors');
+const morgan = require('morgan');
+app.use(morgan('dev'));
 app.use(cors());
 
 app.use(express.static(__dirname + '/../public'));
+app.use('/:productId', express.static(__dirname + '/../public'))
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
